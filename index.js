47b3419dea0dc228a6ec2276a43e71352b86b517
Joshua Westerheide <dev@jdoubleu.de>
2016-12-02 21:53:19 +0100

    Added helper functions: string to PascalCase, get author and email from author string.

--- index.js
+++ index.js
@@ -20,4 +20,40 @@ module.exports = generators.Base.extend({
 		// Calling the super constructor is important so our generator is correctly set up
 		generators.Base.apply(this, arguments);
 	},
+
+	/*
+	 * ---------------------- Helper functions ----------------------
+	 */
+
+	/**
+	 * Converts a string to UpperCamelCase (aka. PascalCase)
+	 * @return String
+	 */
+	_uppercc: function(string) {
+		return _.upperFirst(_.camelCase(string));
+	},
+
+	/**
+	 * Returns the name of an author string
+	 *
+	 * @param author author string (e.g. John Doe <jd@example.com>)
+	 * @returns String
+	 * @private
+	 */
+	_getAuthorName(author) {
+		let name = author.match(/^([^\(<]+)/);
+		return (name && name[1]) ? name[1].trim() : "";
+	},
+
+	/**
+	 * Returns the email of an author string
+	 *
+	 * @param author author string (e.g. John Doe <jd@example.com>)
+	 * @returns String
+	 * @private
+	 */
+	_getAuthorMail(author) {
+		let mail = author.match(/<([^>]+)>/);
+		return (mail && mail[1]) ? mail[1].trim() : "";
+	}
 });
